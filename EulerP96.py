7c67c05fdf570c8119dd5095271abf2383a4bf42
Andrew Fischer <phobosv3@gmail.com>
2019-04-22 15:02:40 -0700

    Update EulerP96.py

--- EulerP96.py
+++ EulerP96.py
@@ -1,5 +1,14 @@
 #Euler P96
 #Reads sudolu puzzles from a text file, solves them, then sums the top left 3 digit numbers for all puzzles
+
+#define the solve a puzzle function that returns the first 3 digit number
+def solve(puzzle):
+    for y in puzzle:
+        for x in y:
+            possible numbers = [1,2,3,4,5,6,7,8,9]
+            
+
+
 with open ("C:\\Users\\Andrew\\Desktop\\p096_sudoku.txt") as file:
     lines = file.readlines()
 
@@ -17,10 +26,13 @@ with open ("C:\\Users\\Andrew\\Desktop\\p096_sudoku.txt") as file:
         for num in lines[i].strip():
             row.append(int(num))
             
-    
         puzzle.append(row)
-        
-    #print(puzzles)
-    print(puzzles[1])
 
+    total = 0
+
+    #go through each puzzle solve it and add the first three digit number to total
+   """for n in puzzles:
+
+        total += solve(puzzle[n])
+    """
     file.close()
